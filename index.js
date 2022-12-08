54f3e55e48cced91b4215a45072a9eda3eb1e517
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-08 10:52:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+console.log(newHeader);
+document.body.append(newHeader);
